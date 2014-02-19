49aacc52ebf2f700aa601f95511032889c0b15cf
Matthieu Keller <keller.mdpa@gmail.com>
2014-02-19 21:35:20 +0100

    remove SourceCodePro font

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,4 @@ ln -s ~/dotfiles/urxvt/ext/clipboard ~/.urxvt/ext/clipboard
 ln -s ~/dotfiles/urxvt/ext/font-size ~/.urxvt/ext/font-size
 xkbcomp -w9 ~/dotfiles/lafayette.xkb $DISPLAY
 xrdb -load ~/.Xresources
-mkdir ~/.fonts/SourceCodePro
-cp ~/dotfiles/fonts/SourceCodePro/* ~/.fonts/SourceCodePro/
 ln -s ~/dotfiles/xinitrc ~/.xinitrc
