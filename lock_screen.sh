7b014bb3171d40334526324e9abadbad3c43bcf0
maggick <maggick@users.noreply.github.com>
2021-06-07 14:44:33 +0200

    i3: Pause spotify on lock screen, resume on unlock

--- lock_screen.sh
+++ lock_screen.sh
@@ -1,4 +1,8 @@
 scrot /tmp/screen_locked.png
 convert /tmp/screen_locked.png -scale 5% -scale 2000% /tmp/screen_locked.png
 cmus-remote -U
-i3lock -i /tmp/screen_locked.png -n; cmus-remote -u; rm /tmp/screen_locked.png
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause
+i3lock -i /tmp/screen_locked.png -n
+cmus-remote -p
+rm /tmp/screen_locked.png
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play
