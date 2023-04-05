730e4dad1c4413fc025f15e96a32f9928448a04f
maggick <maggick@users.noreply.github.com>
2023-04-05 11:10:22 +0200

    Add ltex-ls to vim config (spell/grammar checker)

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -338,7 +338,7 @@ end
 -- nvim-cmp supports additional completion capabilities
 -- Enable the following language servers
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local servers = { 'bashls', 'kotlin_language_server', 'clangd', 'dockerls', 'dartls', 'gopls', 'jdtls', 'pyright', 'rust_analyzer', 'texlab', 'tsserver' }
+local servers = { 'bashls', 'kotlin_language_server', 'clangd', 'dockerls', 'dartls', 'gopls', 'jdtls', 'ltex', 'pyright', 'rust_analyzer', 'texlab', 'tsserver' }
 for _, lsp in ipairs(servers) do
   require('lspconfig')[lsp].setup {
     capabilities = capabilities
