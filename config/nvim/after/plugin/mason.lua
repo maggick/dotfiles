feddf966d44f8a389bda7dec2d20030bbfb1e1ef
maggick <maggick@users.noreply.github.com>
2023-06-27 14:55:12 +0200

    Migrate to mason

---
+++ config/nvim/after/plugin/mason.lua
@@ -0,0 +1,10 @@
+require("mason").setup {
+    ui = {
+        icons = {
+            package_installed = "✓"
+        }
+    }
+}
+require("mason-lspconfig").setup {
+    ensure_installed = { 'bashls', 'kotlin_language_server', 'clangd', 'dockerls', 'jdtls', 'ltex', 'pyright', 'rust_analyzer', 'texlab'},
+}
