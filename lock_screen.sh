855fb66c83caa3b4997ee41c2abdcf65f12cdf57
maggick <maggick@users.noreply.github.com>
2024-02-01 16:25:11 +0100

    Lockscreen script update

--- lock_screen.sh
+++ lock_screen.sh
@@ -1,8 +1,7 @@
 scrot /tmp/screen_locked.png
-convert /tmp/screen_locked.png -scale 5% -scale 2000% /tmp/screen_locked.png
-cmus-remote -U
+convert /tmp/screen_locked.png -scale 5% -scale 2000% /tmp/screen_locked_.png
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause
-i3lock -i /tmp/screen_locked.png -n
-cmus-remote -p
+i3lock -i /tmp/screen_locked_.png -n
 rm /tmp/screen_locked.png
+rm /tmp/screen_locked_.png
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play
