f405a6d0a71832cb6c25d231e1c28dded8133666
maggick <maggick@users.noreply.github.com>
2024-02-09 15:03:41 +0100

    Add more aliases to fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,14 +8,16 @@ end
 
 # Aliases
 alias vim="nvim"                                  # neovim
+alias vi="nvim"                                  # neovim
+alias v="nvim"                                  # neovim
 alias ls="lsd"                                    # lsd
-alias lafayette='setxkbmap fr -variant lafayette' # layout
 alias df='pydf'                                   # pydf
 alias top='btop'                                  # btop
 alias htop='btop'                                 # btop
+alias cat='bat'                                 # btop
 alias keep='echo $(history -p !!) >>~/dotfiles/bash_keeper.txt' # linux command keeper
 
 #define the default editor as vim
 export EDITOR=nvim
 export SUDO_EDITOR=vim
-/usr/bin/rtx activate fish | source
+#/usr/bin/rtx activate fish | source
