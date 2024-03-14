cf39bff9028ea1e24ba99acb7e7eeb21aae321a3
maggick <maggick@users.noreply.github.com>
2024-03-14 18:10:47 +0100

    no more spotifyd

--- lock_screen_wayland.sh
+++ lock_screen_wayland.sh
@@ -1,7 +1,7 @@
 grim /tmp/screen_locked.png
 convert /tmp/screen_locked.png -scale 5% -scale 2000% /tmp/screen_locked_.png
-dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotifyd.instance$(pgrep spotifyd) /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause
 swaylock -i /tmp/screen_locked_.png -n
 rm /tmp/screen_locked.png
 rm /tmp/screen_locked_.png
-dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotifyd.instance$(pgrep spotifyd) /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play
