10a6814765d14f2b605c338259aed21a12fc8e53
maggick <maggick@users.noreply.github.com>
2024-03-28 09:22:24 +0100

    add marp config

---
+++ config/nvim/after/plugin/marp.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>ll", "<cmd>MarpToggle<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>lc", "<cmd>MarpStatus<cr>", { noremap = true, silent = true })
